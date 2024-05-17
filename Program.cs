8037318db746d702a11ee52c032a64f75cedf6b1
DenSad81 <tagil81@mail.ru>
2024-05-17 15:14:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ProgrammUnderThePassword
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string savePassword = "pass";
+            string inputPassword;
+            int quantityOfTry = 3;
+            string secretMassge = "Будь клевым!";
+
+            for (int i = 0; i < quantityOfTry; i++)
+            {
+                Console.Write("Введите пароль: ");
+                inputPassword = Console.ReadLine();
+
+                if (inputPassword == savePassword)
+                {
+                    Console.WriteLine(secretMassge);
+                    i = int.MaxValue;
+                }
+            }
+        }
+    }
+}
